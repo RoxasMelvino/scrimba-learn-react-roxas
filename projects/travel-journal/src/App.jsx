ba9da26c8f23e8d8a84a83a556dd170feb1352ab
RoxasMelvino <melvinoroxasbvt2024@gmail.com>
2024-12-13 11:43:55 -0800

    fix

--- projects/travel-journal/src/App.jsx
+++ projects/travel-journal/src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from "./components/Navbar.jsx"
+import Journal from "./components/Journal.jsx"
+import data from "./data.js"
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const articles = data.map(item => {
+    return (
+      <Journal
+        key={item.id}
+        {...item}
+      />
+    )
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="page">
+      <Navbar />
+      <section className="journal__section">
+        {articles}
+      </section>
+    </div>
   )
 }
 
--- projects/travel-journal/src/components/Journal.jsx
+++ projects/travel-journal/src/components/Journal.jsx
@@ -2,17 +2,14 @@ import pin from "/public/pin.png"
 
 
 export default function Journal(props) {
-
-    // console.log(props);
     
     return (
         <article className="journal__article">
-            <img src="https://picsum.photos/id/237/200/300" alt="Photo of the place" className="article__img"/>
+            <img src={``} alt="Photo of the place" className="article__img"/>
             <header>
                 <div className="header__div--flex">
-                    <img src={pin} alt="pin location marker"/>
-                    <p>Location</p>
-                    <a href="linkhere">View on Google Maps</a>
+                    <p className="header__location"><img src={pin} alt="pin location marker" className="header__div-pin"/> LOCATION </p>
+                    <a href="linkhere" className="header__link">View on Google Maps</a>
                 </div>
                 <h1>Title</h1>
                 <p><strong>StartDate - EndDate</strong></p>
