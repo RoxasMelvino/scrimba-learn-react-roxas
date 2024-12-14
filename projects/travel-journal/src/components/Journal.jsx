bfa71e9b5d67aa56bbd9db209f111f002b99f18d
RoxasMelvino <melvinoroxasbvt2024@gmail.com>
2024-12-14 10:00:29 -0800

    finish travel journal

--- projects/travel-journal/src/components/Journal.jsx
+++ projects/travel-journal/src/components/Journal.jsx
@@ -5,15 +5,15 @@ export default function Journal(props) {
     
     return (
         <article className="journal__article">
-            <img src={``} alt="Photo of the place" className="article__img"/>
+            <img src={props.imageUrl} alt="Photo of the place" className="article__img"/>
             <header>
-                <div className="header__div--flex">
-                    <p className="header__location"><img src={pin} alt="pin location marker" className="header__div-pin"/> LOCATION </p>
-                    <a href="linkhere" className="header__link">View on Google Maps</a>
+                <div className="header__location-info">
+                    <p className="header__location"><img src={pin} alt="pin location marker" className="header__div-pin"/> {props.location.toUpperCase()} </p>
+                    <a href={props.googleMapsUrl} className="header__link">View on Google Maps</a>
                 </div>
-                <h1>Title</h1>
-                <p><strong>StartDate - EndDate</strong></p>
-                <p>Description</p>
+                <h1 className="header__title">{props.title}</h1>
+                <p><strong>{props.startDate} - {props.endDate}</strong></p>
+                <p>{props.description}</p>
             </header>
         </article>
     )
--- projects/travel-journal/src/data.js
+++ projects/travel-journal/src/data.js
@@ -7,7 +7,7 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        imageUrl: "https://images.unsplash.com/photo-1589308078059-be1415eab4c3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDIwNjJ8MHwxfGFsbHx8fHx8fHx8fDE3MjMyMzUwNjR8&ixlib=rb-4.0.3&q=80&w=1080"
     },
     {
         id: 2,
@@ -17,16 +17,16 @@ export default [
         startDate: "27 May 2021",
         endDate: "8 Jun 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
-        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+        imageUrl: "https://images.unsplash.com/photo-1624138784614-87fd1b6528f8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDIwNjJ8MHwxfGFsbHx8fHx8fHx8fDE3MjMyMzUyNjF8&ixlib=rb-4.0.3&q=80&w=1080"
     },
     {
         id: 3,
         title: "Geirangerfjord",
         location: "Norway",
         googleMapsUrl: "https://goo.gl/maps/uZathFh1WRMjVLpt7",
-        startDate: "01 Oct, 2021",
-        endDate: "18 Nov, 2021",
+        startDate: "01 Oct 2021",
+        endDate: "18 Nov 2021",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+        imageUrl: "https://images.unsplash.com/photo-1601439678777-b2b3c56fa627?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDIwNjJ8MHwxfGFsbHx8fHx8fHx8fDE3MjMyMzUzMTF8&ixlib=rb-4.0.3&q=80&w=1080"
     }
 ]
