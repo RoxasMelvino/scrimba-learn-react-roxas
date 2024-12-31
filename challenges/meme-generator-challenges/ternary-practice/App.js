b88e3356286337807073ed4941773f89755a5544
RoxasMelvino <melvinoroxasbvt2024@gmail.com>
2024-12-31 08:02:27 -0800

    finish ternary operator challenge

--- challenges/meme-generator-challenges/ternary-practice/App.js
+++ challenges/meme-generator-challenges/ternary-practice/App.js
@@ -2,23 +2,32 @@ import React from "react"
 
 export default function App() {
     /**
-     * Challenge: Replace the if/else below with a ternary
+     * Challenge1 : Replace the if/else below with a ternary
      * to determine the text that should display on the page
+     * 
+     * Challenge2: move our ternary directly inside of the JSX
+     * so the "Yes" and "No" are determined inside the <h1>
+     * 
+     * Hint: you will no longer need the `answer` variable
      */
+
     const isGoingOut = true
     
-    let answer  // Use ternary here
-    if(isGoingOut === true) {
-        answer = "Yes"
-    } else {
-        answer = "No"
-    }
+    // let answer 
+    // if(isGoingOut === true) {
+    //     answer = "Yes"
+    // } else {
+    //     answer = "No"
+    // }
+
+    // let answer = isGoingOut ? "Yes" : "No";
+    // console.log(answer);
     
     return (
         <div className="state">
             <h1 className="state--title">Do I feel like going out tonight?</h1>
             <div className="state--value">
-                <h1>{answer}</h1>
+                <h1>{isGoingOut ? "Yes": "No"}</h1>
             </div>
         </div>
     )
--- projects/meme-generator/src/components/Meme.jsx
+++ projects/meme-generator/src/components/Meme.jsx
@@ -1,26 +1,33 @@
 import memesData from "../memesData.js"
+import { useState } from 'react';
 
 export default function Meme() {
-    const memes = memesData.data.memes
+    const memes = memesData.data.memes;
+    const [image, setImage] = useState('');
 
     function getRandomImage() {
         const randIdx = Math.floor(Math.random() * memes.length)
         const url = memes[randIdx].url
-        console.log(url, randIdx);
+        setImage(url);
     }
 
     
     return (
-        <div className="form">
-            <label className="form__text-label" >
-                <p>Top Text</p>
-                <input type="text" className="form__input-text"required/>
-            </label>
-            <label className="form__text-label">
-                <p>Bottom Text</p>
-                <input type="text" className="form__input-text"required/>
-            </label>
-            <button className="form__submit-btn" onClick={getRandomImage}>Get a new meme image 🖼️</button>
-        </div>
+        <main>
+            <div className="form">
+                <label className="form__text-label" >
+                    <p>Top Text</p>
+                    <input type="text" className="form__input-text"required/>
+                </label>
+                <label className="form__text-label">
+                    <p>Bottom Text</p>
+                    <input type="text" className="form__input-text"required/>
+                </label>
+                <button className="form__submit-btn" onClick={getRandomImage}>Get a new meme image 🖼️</button>
+            </div>
+            <div className="image">
+                <img src={image} alt="meme image" className="image__meme" />
+            </div>
+        </main>
     )
 }
